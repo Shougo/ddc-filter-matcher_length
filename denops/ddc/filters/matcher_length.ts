73ef114043b1090a68edd7ce1f80ff92b6cab149
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-22 15:21:28 +0900

    Initial version

---
+++ denops/ddc/filters/matcher_length.ts
@@ -0,0 +1,17 @@
+import {
+  BaseFilter,
+  Candidate,
+  SourceOptions,
+} from "https://deno.land/x/ddc_vim@v0.2.2/types.ts#^";
+
+export class Filter extends BaseFilter {
+  filter(args: {
+    sourceOptions: SourceOptions,
+    completeStr: string,
+    candidates: Candidate[],
+  }): Promise<Candidate[]> {
+    return Promise.resolve(args.candidates.filter(
+      (candidate) => candidate.word.length > args.completeStr.length
+    ));
+  }
+}
