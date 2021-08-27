93e126471d1437fae31475ebe2b613447ab1344e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-27 22:49:39 +0900

    ddc.vim version up

--- denops/ddc/filters/matcher_length.ts
+++ denops/ddc/filters/matcher_length.ts
@@ -2,7 +2,7 @@ import {
   BaseFilter,
   Candidate,
   SourceOptions,
-} from "https://deno.land/x/ddc_vim@v0.2.2/types.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.3.0/types.ts#^";
 
 export class Filter extends BaseFilter {
   filter(args: {
