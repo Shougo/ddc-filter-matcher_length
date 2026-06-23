5a0b0391d65e2fb5008bfa387e33a24347bd6bb3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2026-06-24 07:07:48 +0900

    Simplify

--- denops/@ddc-filters/matcher_length/main.ts
+++ denops/@ddc-filters/matcher_length/main.ts
@@ -1,17 +1,16 @@
-import type { Item, SourceOptions } from "@shougo/ddc-vim/types";
+import type { Item } from "@shougo/ddc-vim/types";
 import { BaseFilter } from "@shougo/ddc-vim/filter";
 
 type Params = Record<string, never>;
 
 export class Filter extends BaseFilter<Params> {
   override filter(args: {
-    sourceOptions: SourceOptions;
     completeStr: string;
     items: Item[];
-  }): Promise<Item[]> {
-    return Promise.resolve(args.items.filter(
+  }): Item[] {
+    return args.items.filter(
       (item) => item.word.length > args.completeStr.length,
-    ));
+    );
   }
 
   override params(): Params {
